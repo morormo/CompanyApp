4739db470ac27ac055646bc28f0c6e2c4adbb99a
Morormo <morormo@gmail.com>
2020-05-24 16:35:45 +0200

    ADD populate to employees

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const ObjectId = require('mongodb').ObjectId;
-const Employee = require('../models/department.model');
+const Employee = require('../models/employee.model');
 
 router.get('/employees', async (req, res) => {
   try {
-    res.json(await Employee.find());
+    res.json(await Employee.find().populate('department'));
   }
   catch(err) {
     res.status(500).json({ message: err });
@@ -17,7 +17,7 @@ router.get('/employees/random', async (req, res) => {
   try {
     const count = await Employee.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const dep = await Employee.findOne().skip(rand);
+    const dep = await (await Employee.findOne().skip(rand)).populate('department');
     if(!dep) res.status(404).json({ message: 'Not found' });
     else res.json(dep);
   }
@@ -30,7 +30,7 @@ router.get('/employees/random', async (req, res) => {
 router.get('/employees/:id', async (req, res) => {
 
   try {
-    const dep = await Employee.findById(req.params.id);
+    const dep = await (await Employee.findById(req.params.id)).populate('department');
     if(!dep) res.status(404).json({ message: 'Not found' });
     else res.json(dep);
   }
